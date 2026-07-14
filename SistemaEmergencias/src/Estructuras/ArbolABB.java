9045d509da8b6627828c91c204798ab164eaab01
Jhoaos <jhare24jhoao@gmail.com>
2026-07-14 13:44:21 -0500

    Refactor ArbolABB class structure and formatting

--- SistemaEmergencias/src/Estructuras/ArbolABB.java
+++ SistemaEmergencias/src/Estructuras/ArbolABB.java
@@ -5,23 +5,16 @@ import modelo.Paciente;
 public class ArbolABB {
 
     private NodoAbb raiz;
-
     public ArbolABB() {
-
         raiz = null;
     }
-    
     public void insertar(Paciente paciente) {
-
         raiz = insertarRecursivo(raiz, paciente);
     }
-    
     private NodoAbb insertarRecursivo(NodoAbb actual, Paciente paciente) {
-
         if (actual == null) {
             return new NodoAbb(paciente);
         }
-
         int dniNuevo = Integer.parseInt(paciente.getDni());
         int dniActual = Integer.parseInt(actual.paciente.getDni());
         if (dniNuevo < dniActual) {
@@ -37,32 +30,20 @@ public class ArbolABB {
     }
     
     public Paciente buscar(String dni) {
-
         return buscarRecursivo(raiz, Integer.parseInt(dni));
     }
-
     private Paciente buscarRecursivo(NodoAbb actual, int dni) {
-
         if (actual == null) {
-
             return null;
-
         }
-
         int dniActual = Integer.parseInt(actual.paciente.getDni());
-
         if (dni == dniActual) {
-
             return actual.paciente;
-
         }
-
         if (dni < dniActual) {
-
             return buscarRecursivo(actual.izquierda, dni);
 
         }
-
         return buscarRecursivo(actual.derecha, dni);
 
     }
